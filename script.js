e94c3ea2c636e18745e8b6583a063a564a7a9a84
aisha <aisha@tesla.com>
2024-03-27 21:32:19 +0400

    hsdgfgsdf

--- script.js
+++ script.js
@@ -4,6 +4,32 @@ const movieDB = {
         "Titanic",
         "Bəyin oğurlanması",
         "Xaç atası",
-        "Avatar"
+        "Avatar", 
+        "Inspection"
     ]
 }
+
+const reklam = document.querySelectorAll('.promo_adv img')
+const janr = document.querySelector('.promo_genre')
+const sekil = document.querySelector('.promo_bg')
+const kinolar = document.querySelector('.promo_interactive-list')
+
+
+reklam.forEach(e=> {
+    e.remove()
+});
+
+janr.innerHTML = 'Drama';
+
+sekil.style.backgroundImage = 'url(img/bg.jpg)';
+
+kinolar.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((kino, index) => {
+    kinolar.innerHTML +=`
+    <li class="promo_interactive-item">${index+1}. ${kino}
+    <div class="delete"></div>
+</li>`
+});
